b20ab32a44b2e7aec7bcc66963326a9592e07595
Mark Striemer <mstriemer@mozilla.com>
2015-05-17 10:54:54 -0500

    Simple chat between tabs/windows

---
+++ static/js/broadcaster.js
@@ -0,0 +1,22 @@
+var users = [];
+
+onconnect = function (e) {
+	var port = e.ports[0];
+
+	port.onmessage = function (e) {
+		if (e.data.register) {
+			users.push({
+				username: e.data.register,
+				port: port,
+			});
+		} else if (e.data.message && e.data.sender) {
+			users.forEach(function (user) {
+				if (user.username !== e.data.sender) {
+					user.port.postMessage(e.data);
+				}
+			});
+		}
+	};
+
+	port.start();
+};
