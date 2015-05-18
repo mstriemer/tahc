82a4b0f7b50604c6df67bde0e969bc3c16c434cc
Mark Striemer <mstriemer@mozilla.com>
2015-05-18 12:53:22 -0500

    Single tab react version

--- static/js/tahc-react.js
+++ static/js/tahc-react.js
@@ -3,33 +3,65 @@ var TahcApp = React.createClass({
     return {messages: []};
   },
   render: function () {
-    var messages = this.state.messages.map(function (message) {
+    if (this.state.sender) {
+      return <div>
+        <TahcMessages messages={this.state.messages} />
+        <TahcSend sendMessage={this.sendMessage} />
+      </div>;
+    } else {
+      return <TahcRegister signIn={this.signIn} />
+    }
+  },
+  sendMessage: function (message) {
+    this.state.messages.push({
+      sender: this.state.sender,
+      message: message,
+      sentAt: new Date(),
+    });
+    this.setState({messages: this.state.messages});
+  },
+  signIn: function (username) {
+    this.setState({sender: username});
+  },
+});
+
+var TahcMessages = React.createClass({
+  render: function () {
+    var messages = this.props.messages.map(function (message) {
       return <pre>{JSON.stringify(message)}</pre>;
     });
-    console.log(messages);
-    return <div>
-      <div class="messages">{messages}</div>
-      <form onSubmit={this.handleNewMessage}>
-        <input type="text" ref="message" autoFocus={true} />
-        <button type="submit">Send</button>
-      </form>
-    </div>;
+    return <div>{messages}</div>;
+  },
+});
+
+var TahcSend = React.createClass({
+  render: function () {
+    return <form onSubmit={this.handleNewMessage}>
+      <input type="text" ref="message" autoFocus={true} required />
+      <button type="submit">Send</button>
+    </form>;
   },
   handleNewMessage: function (e) {
     e.preventDefault();
-    
-    var messageNode = this.refs.message.getDOMNode();
 
-    this.state.messages.push({
-      sender: this.props.sender,
-      message: messageNode.value,
-      sentAt: new Date(),
-    });
-    
-    this.setState(this.state);
-    
+    var messageNode = this.refs.message.getDOMNode();
+    this.props.sendMessage(messageNode.value);
     messageNode.value = '';
   },
 });
 
-React.render(<TahcApp sender="mark" />, document.body);
+var TahcRegister = React.createClass({
+  render: function () {
+    return <form onSubmit={this.handleSignIn}>
+      <input type="text" ref="username" autoFocus={true} required />
+      <button type="submit">Sign In</button>
+    </form>;
+  },
+  handleSignIn: function (e) {
+    e.preventDefault();
+
+    this.props.signIn(this.refs.username.getDOMNode().value);
+  }
+})
+
+React.render(<TahcApp />, document.body);
